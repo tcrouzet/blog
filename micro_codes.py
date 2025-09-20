75a65b1cdbd808a448c9c58a24826a6993e5d093
Thierry Crouzet <tc@tcrouzet.com>
2025-09-20 21:37:45 +0200

    Auto-commit uncommited files - 2025-09-20 21:37:45

--- micro_codes.py
+++ micro_codes.py
@@ -43,7 +43,7 @@ class MicroCodes:
             frontmatter = p.get("frontmatter", None)
             if frontmatter:
                 genre = frontmatter.get("genre","autre").lower()
-                book = f'<a href="{p.get("url")}"><em>{html.escape(p.get("title"))}</em></a>, {frontmatter.get("date").year}<br/>'
+                book = f'<a href="{html.escape(p.get("url","/biliographie/"))}"><em>{html.escape(p.get("title"))}</em></a>, {frontmatter.get("date").year}<br/>'
                 if genre == "roman":
                     romman += book
                 elif genre == "imaginaire":
